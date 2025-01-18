4e18e9e3026b02680a692e71f6da4cdb1544f6fb
sukhdeepdeveloper05 <sukhdeepdeveloper05@gmail.com>
2025-01-18 18:08:24 +0530

    updated

--- src/app/faqs/page.jsx
+++ src/app/faqs/page.jsx
@@ -10,7 +10,7 @@ export default function FAQsPage() {
   return (
     <main className="page-content mt-headerHeight">
       <section className="grid md:grid-cols-2 md:py-20 max-md:max-w-[85%] max-md:mx-auto">
-        <h3 className="md:text-center md:mb-0 mb-12 text-4xl">FAQs</h3>
+        <h3 className="md:text-center md:mb-0 my-8 text-4xl">FAQs</h3>
         <div className="flex flex-col md:gap-16 gap-8 md:max-w-[85%] max-md:mx-auto">
           {faqs.map((faq, index) => {
             return (
